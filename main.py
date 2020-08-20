b10d528cfd5866651c79d3305e54464f7958df72
NHummel76 <45400450+NiHummel@users.noreply.github.com>
2020-08-20 19:38:59 +0300

    Gaussian filter, using numpy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import cv2
 import sys
 import numpy as np
-import time
 
 # preprocessed weights of gaussian kernel with sigma = 1 and sigma = 2
 gaussian_1D_kernel_with_sigma_1 = np.asarray([30, 131, 216, 131, 30], dtype=np.uint8)
@@ -10,9 +9,9 @@ gaussian_1D_kernel_with_sigma_2 = np.asarray([147, 213, 242, 213, 147], dtype=np
 
 def gaussian_filter(image, sigma=1):
     kernel = gaussian_1D_kernel_with_sigma_1 if sigma == 1 else gaussian_1D_kernel_with_sigma_2
-    normalizer = 0
-    for i in kernel:
-        normalizer += i
+    normalizer = [int(kernel[0])]
+    for i in range(1, 5):
+        normalizer.append(normalizer[i - 1] + kernel[i])
     height, weight = image.shape[0], image.shape[1]
 
     # horizontal blur
@@ -23,7 +22,7 @@ def gaussian_filter(image, sigma=1):
             new_pixel = 0
             for ind in range(y_min, y_max):
                 new_pixel += image[i, ind] * int(kernel[ind - y_min])
-            blurred_horizontal_image[i, j] = new_pixel // normalizer
+            blurred_horizontal_image[i, j] = new_pixel // normalizer[y_max - y_min - 1]
 
     # vertical blur
     blurred_image = np.ndarray(shape=(height, weight), dtype=np.uint8)
@@ -33,13 +32,13 @@ def gaussian_filter(image, sigma=1):
             new_pixel = 0
             for ind in range(x_min, x_max):
                 new_pixel += image[ind, j] * int(kernel[ind - x_min])
-            blurred_image[i, j] = new_pixel // normalizer
+            blurred_image[i, j] = new_pixel // normalizer[x_max - x_min - 1]
 
     return blurred_image
 
 
 def main():
-    assert len(sys.argv) - 1 == 1, 'Wrong amount of arguments'
+    assert len(sys.argv) - 1 == 1, 'Wrong amount of arguments. \nOne argument was expected: source image name.'
 
     img_name = sys.argv[1]
 
