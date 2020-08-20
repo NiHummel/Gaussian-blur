9c3c48ee75a712aeb3c662f7789d2ba1adddd384
NHummel76 <45400450+NiHummel@users.noreply.github.com>
2020-08-20 15:54:12 +0300

    Gaussian filter, using numpy

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import cv2
 import sys
 import numpy as np
+import time
 
-# preprocessed weights of gaussian kernel with sigma = 1
-filter14 = np.array([[30, 131, 216, 131, 30],
-                     [131, 586, 966, 586, 131],
-                     [216, 966, 1592, 966, 216],
-                     [131, 586, 966, 586, 131],
-                     [30, 131, 216, 131, 30]])
+# preprocessed weights of gaussian kernel with sigma = 1 and sigma = 2
+gaussian_1D_kernel_with_sigma_1 = np.asarray([30, 131, 216, 131, 30], dtype=np.uint8)
+gaussian_1D_kernel_with_sigma_2 = np.asarray([147, 213, 242, 213, 147], dtype=np.uint8)
 
-# preprocessed weights of gaussian kernel with sigma = 2
-filter24 = np.array([[147, 213, 242, 213, 147],
-                     [213, 310, 352, 310, 213],
-                     [242, 352, 398, 352, 242],
-                     [213, 310, 352, 310, 213],
-                     [147, 213, 242, 213, 147]])
+
+def gaussian_filter(image, sigma=1):
+    kernel = gaussian_1D_kernel_with_sigma_1 if sigma == 1 else gaussian_1D_kernel_with_sigma_2
+    normalizer = 0
+    for i in kernel:
+        normalizer += i
+    height, weight = image.shape[0], image.shape[1]
+
+    # horizontal blur
+    blurred_horizontal_image = np.ndarray(shape=(height, weight), dtype=np.uint8)
+    for i in range(height):
+        for j in range(weight):
+            y_min, y_max = max(0, j - 2), min(weight, j + 3)
+            new_pixel = 0
+            for ind in range(y_min, y_max):
+                new_pixel += image[i, ind] * int(kernel[ind - y_min])
+            blurred_horizontal_image[i, j] = new_pixel // normalizer
+
+    # vertical blur
+    blurred_image = np.ndarray(shape=(height, weight), dtype=np.uint8)
+    for i in range(height):
+        x_min, x_max = max(0, i - 2), min(height, i + 3)
+        for j in range(weight):
+            new_pixel = 0
+            for ind in range(x_min, x_max):
+                new_pixel += image[ind, j] * int(kernel[ind - x_min])
+            blurred_image[i, j] = new_pixel // normalizer
+
+    return blurred_image
 
 
 def main():
@@ -27,32 +48,9 @@ def main():
     except FileNotFoundError as e:
         raise e
 
-    assert image.shape == (1080, 1920), 'Wrong shape of image'
-    cv2.imwrite(f'grayscale.jpg', image)
-
-    image14, image24 = [], []
-    norm14, norm24 = np.sum(filter14), np.sum(filter24)
-
-    for i in range(1080):
-        row14, row24 = [], []
-        xmi, xma = max(0, i - 2), min(1080, i + 3)
-        for j in range(1920):
-            ymi, yma = max(0, j - 2), min(1920, j + 3)
-            sub_img = image[xmi:xma, ymi:yma]
-            if sub_img.shape == (5, 5):
-                row14.append(np.sum(np.multiply(sub_img, filter14) // norm14))
-                row24.append(np.sum(np.multiply(sub_img, filter24) // norm24))
-                continue
-            xa, xb, ya, yb = 2 - (i - xmi), 2 + (xma - i), 2 - (j - ymi), 2 + (yma - j)
-            kernel14 = filter14[xa:xb, ya:yb]
-            kernel24 = filter24[xa:xb, ya:yb]
-            row14.append(np.sum(np.multiply(sub_img, kernel14) // norm14))
-            row24.append(np.sum(np.multiply(sub_img, kernel24) // norm24))
-        image14.append(row14)
-        image24.append(row24)
-
-    cv2.imwrite('blurred1.jpg', np.asarray(image14, dtype=np.uint8))
-    cv2.imwrite('blurred2.jpg', np.asarray(image24, dtype=np.uint8))
+    cv2.imwrite('grayscale.jpg', image)
+    cv2.imwrite('blurred1.jpg', gaussian_filter(image, sigma=1))
+    cv2.imwrite('blurred2.jpg', gaussian_filter(image, sigma=2))
 
 
 if __name__ == '__main__':
